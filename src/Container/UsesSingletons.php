4f265cf239d0f4da88ee4831ca7ce1d095df2743
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-06-27 13:50:57 +0800

    Implement UsesSingletons trait: provide methods for managing singleton instances and factories for improved dependency management

---
+++ src/Container/UsesSingletons.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Rcalicdan\FiberAsync\Container;
+
+trait UsesSingletons
+{
+    private static array $singletonInstances = [];
+    private static array $singletonFactories = [];
+
+    protected function singleton(string $className, ?callable $factory = null): object
+    {
+        if (!isset(self::$singletonInstances[$className])) {
+            if ($factory !== null) {
+                self::$singletonFactories[$className] = $factory;
+            }
+
+            if (isset(self::$singletonFactories[$className])) {
+                self::$singletonInstances[$className] = self::$singletonFactories[$className]();
+            } elseif (class_exists($className)) {
+                self::$singletonInstances[$className] = new $className();
+            } else {
+                throw new \InvalidArgumentException("Class {$className} does not exist");
+            }
+        }
+        return self::$singletonInstances[$className];
+    }
+
+    protected function bindSingleton(string $className, callable $factory): void
+    {
+        self::$singletonFactories[$className] = $factory;
+        // Remove existing instance to force recreation with new factory
+        unset(self::$singletonInstances[$className]);
+    }
+
+    protected function fresh(string $className, ?callable $factory = null): object
+    {
+        if ($factory !== null) {
+            return $factory();
+        }
+        
+        if (isset(self::$singletonFactories[$className])) {
+            return self::$singletonFactories[$className]();
+        }
+        
+        if (class_exists($className)) {
+            return new $className();
+        }
+        
+        throw new \InvalidArgumentException("Class {$className} does not exist");
+    }
+
+    protected function clearSingletons(): void
+    {
+        self::$singletonInstances = [];
+        self::$singletonFactories = [];
+    }
+
+    protected function hasSingleton(string $className): bool
+    {
+        return isset(self::$singletonInstances[$className]);
+    }
+}
